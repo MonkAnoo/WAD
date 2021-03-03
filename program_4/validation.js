b240689be878f6a599876b016960f5e5c580c4f7
anish sachin <sachinabs.js@gmail.com>
2021-03-03 10:51:05 +0530

    later

--- program_4/validation.js
+++ program_4/validation.js
@@ -15,21 +15,34 @@ function checkName()
 
 function rollNumberCheck()
 {
-    let rnumber = document.getElementById("rNumber").value;
-    var l = rnumber.length;
-    
+    let rNumber = document.getElementById("rNumber").value;
+    var l = rNumber.length;
+    let alp = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let flagNum=0, flagAlp = 0;
+
     for(let i =0;i<l;i++)
     {
-        let x = parseInt(rnumber);
-        alert(x)
-        // if(!isNaN(x))
-        // {
-        //     alert("");
-        // }
-        // if(Number(rnumber[i]))
-        // {
-        //     alert("nuber");
-        // }
+        if(res = parseInt(rNumber[i]))
+        {
+            flagNum = flagNum +1;
+            console.log("num"+flagNum);
+        }
+        else
+        {
+            for(j=0;j<26;j++)
+            {
+                
+                    if(rNumber[i] === alp[j])
+                    {
+                        flagAlp = flagAlp +1;
+                        console.log("alp" + flagAlp);
+                    }
+                           
+               
+            }
+        }
+       
+       
         
     }
 
